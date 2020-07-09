0ea4042f37fa62840cffa5ccf642415fd757caf0
Кирилл <kirillmorgunov@mail.ru>
2020-07-09 15:06:20 +0300

    реализовал логику меню на мобильной версии

--- source/js/scripts/script.js
+++ source/js/scripts/script.js
@@ -1,6 +1,9 @@
 'use strict';
+console.log("sdfasfsadfsdfsdf");
 var pageHeader = document.querySelector('.page-header');
 var headerToggle = document.querySelector('.page-header__toggle');
+var intro =  document.querySelector('.intro');
+var footer = document.querySelector('.page-footer');
 
 pageHeader.classList.remove('page-header--nojs');
 
@@ -8,8 +11,13 @@ headerToggle.addEventListener('click', function () {
   if (pageHeader.classList.contains('page-header--closed')) {
     pageHeader.classList.remove('page-header--closed');
     pageHeader.classList.add('page-header--opened');
+
+    intro.style.display = "none";
+    footer.style.display = "flex";
   } else {
     pageHeader.classList.add('page-header--closed');
     pageHeader.classList.remove('page-header--opened');
+    intro.style.display = "block";
+    footer.style.display = "none";
   }
 });
