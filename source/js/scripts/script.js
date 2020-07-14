597839206a07a6230038016aab07c13d35a60fdc
Кирилл <kirillmorgunov@mail.ru>
2020-07-14 16:10:32 +0300

    исправил фон на главной странице, добавил фиксированное положение верхнему меню, работа над списков квестов

--- source/js/scripts/script.js
+++ source/js/scripts/script.js
@@ -4,6 +4,8 @@ var pageHeader = document.querySelector('.page-header');
 var headerToggle = document.querySelector('.page-header__toggle');
 var intro =  document.querySelector('.intro');
 var footer = document.querySelector('.page-footer');
+var pageBody =  document.querySelector("body");
+var pageBodyIndex = document.querySelector(".index");
 
 pageHeader.classList.remove('page-header--nojs');
 
@@ -13,12 +15,16 @@ headerToggle.addEventListener('click', function () {
     pageHeader.classList.add('page-header--opened');
 
     headerToggle.style.backgroundImage = "url('../img/close.svg')";
+    pageBody.backgroundImage = "url('../img/menuOpenBg.jpg')";
+    pageBodyIndex.backgroundImage = "url('../img/menuOpenBg.jpg')";
     intro.style.display = "none";
     footer.style.display = "flex";
   } else {
     pageHeader.classList.add('page-header--closed');
     pageHeader.classList.remove('page-header--opened');
 
+    pageBody.backgroundImage = "url('../img/mainBg-mobile.jpg')";
+    pageBodyIndex.backgroundImage = "url('../img/mainBg-mobile.jpg')";
     headerToggle.style.backgroundImage = "url('../img/burger.svg')";
     intro.style.display = "block";
     footer.style.display = "none";
