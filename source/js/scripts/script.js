23661b809c65dafae0d48e25b27b25926fef789c
Кирилл <kirillmorgunov@mail.ru>
2020-07-17 17:53:45 +0300

    исправлен фон у меню на мобильной версии

--- source/js/scripts/script.js
+++ source/js/scripts/script.js
@@ -2,10 +2,8 @@
   "use strict";
 var pageHeader = document.querySelector('.page-header');
 var headerToggle = document.querySelector('.page-header__toggle');
-var intro =  document.querySelector('.intro');
-var footer = document.querySelector('.page-footer');
-var pageBody =  document.querySelector("body");
-var pageBodyIndex = document.querySelector(".index");
+var overlayMenu = document.querySelector(".overlay-menu");
+var footer = document.querySelector(".page-footer");
 
 pageHeader.classList.remove('page-header--nojs');
 
@@ -13,20 +11,14 @@ headerToggle.addEventListener('click', function () {
   if (pageHeader.classList.contains('page-header--closed')) {
     pageHeader.classList.remove('page-header--closed');
     pageHeader.classList.add('page-header--opened');
-
     headerToggle.style.backgroundImage = "url('img/close.svg')";
-    pageBody.backgroundImage = "url('img/menuOpenBg.jpg')";
-    pageBodyIndex.backgroundImage = "url('img/menuOpenBg.jpg')";
-    intro.style.display = "none";
+    overlayMenu.classList.add("modal-show");
     footer.style.display = "flex";
   } else {
     pageHeader.classList.add('page-header--closed');
     pageHeader.classList.remove('page-header--opened');
-
-    pageBody.backgroundImage = "url('img/mainBg-mobile.jpg')";
-    pageBodyIndex.backgroundImage = "url('img/mainBg-mobile.jpg')";
     headerToggle.style.backgroundImage = "url('img/burger.svg')";
-    intro.style.display = "block";
+    overlayMenu.classList.remove("modal-show");
     footer.style.display = "none";
   }
 });
